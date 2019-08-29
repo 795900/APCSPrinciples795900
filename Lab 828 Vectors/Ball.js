932be00faf58bdb785c1bd1497b7faf0c4093fdd
795900 <795900@seq.org>
2019-08-29 08:50:16 -0700

    8/29 edits

--- Lab 828 Vectors/Ball.js
+++ Lab 828 Vectors/Ball.js
@@ -1,9 +1,10 @@
 
 
 class Ball{
-  constructor(x,y,dx,dy){
-    this.loc = createVector(x,y);
+  constructor(x, y, dx, dy){
+    this.loc = createVector(x, y);
     this.vel = createVector(dx,dy);
+    this.acc = createVector(0, 1)
     this.clr = color(random(255),random(255),random(255))
   }
 
@@ -24,15 +25,22 @@ class Ball{
       this.vel.y = -this.vel.y
     }
     if(this.loc.y>height){
-      this.vel.y = this.vel.y
+      this.vel.y = -this.vel.y
+    }
+    if(this.loc.y>height+20){
+      this.reset()
     }
   }
-
+  reset(){
+    this.loc.y = random(height)
+    this.vel.y = 1
+  }
   update(){
+    this.vel.add(this.acc)
     this.loc.add(this.vel)
   }
   render(){
     fill(this.clr);
-    ellipse(this.x, this.y, 50,50)
+    ellipse(this.loc.x, this.loc.y, 20,20)
   }
 }
--- Lab 828 Vectors/sketch.js
+++ Lab 828 Vectors/sketch.js
@@ -1,5 +1,5 @@
-//  Your Name
-// 	Date or version number
+//  Harry Kraus
+// 	8/29
 //  This is a comment
 //  The setup function function is called once when your program begins
 var balls = []
@@ -7,18 +7,18 @@ var balls = []
 function setup() {
   var cnv = createCanvas(800,800);
   cnv.position((windowWidth-width)/2, 30);
-  loadBalls(100)
+  loadBalls(20)
 
 }
 
 function draw() {
-  background(20,20,20);
+  background(20,20,20,50);
   runBalls();
 }
 
 function loadBalls(n){
   for(var i = 0; i < n; i++){
-    balls[i] = new Ball(random(width), random(height), random(-8,8), random(-8,8))
+    balls[i] = new Ball(random(width), random(height), random(-1,1), random(-1,1))
   }
 }
 
