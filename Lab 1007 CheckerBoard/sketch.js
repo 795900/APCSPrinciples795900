3a30f70d2f589058cd24054372ad3a5d33d95095
795900 <795900@seq.org>
2019-10-08 08:51:11 -0700

    Commit 1008 From School

--- Lab 1007 CheckerBoard/sketch.js
+++ Lab 1007 CheckerBoard/sketch.js
@@ -2,9 +2,9 @@
 // 	1007
 //  This is a comment
 //  The setup function function is called once when your program begins
-squareX = 0
-squareY = 0
-squares = []
+var squareX = 0
+var squareY = 0
+var squares = []
 
 function setup() {
   var cnv = createCanvas(800, 800);
@@ -16,23 +16,35 @@ function setup() {
 
 function loadSquares(){
   for (rowNum = 1; rowNum<=height/100; rowNum++){
+    squareX = 0
+    squareY = 0
     if (rowNum % 2 === 0){
       for (i = 0; i < width; i = i+2){
-        squares[i] = new Square (squareX,squareY,250,0,0)
-        squares[i] = new Square (squareX,squareY,20,20,20)
+        squares[i] = new Square (i*100,i*100,250,0,0)
+        squares[i+1] = new Square (100,100(i+1),20,20,20)
+        squareX += 100
+        squareY += 100
+      }
+      for (var i = 0; i < squares.length; i++){
+        squares[i].render();
       }
     }
     if (rowNum % 2 === 1){
+      squareX = 0
+      squareY = 0
       for (i = 0; i < width; i = i+2){
         squares[i] = new Square (squareX,squareY,20,20,20)
-        squares[i] = new Square (squareX,squareY,250,0,0)
+        squares[i+1] = new Square (squareX+100,squareY+100,250,0,0)
+        squareX += 100
+        squareY += 100
+      }
+      for (var i = 0; i < squares.length; i++){
+        squares[i].render();
       }
     }
   }
 }
 //  The draw function is called @ 30 fps
 function draw() {
-  for (var i = 0; i < squares.length; i++){
-    squares[i]
-  }
+
 }
