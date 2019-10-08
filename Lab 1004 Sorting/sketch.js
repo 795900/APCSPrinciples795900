eca3e9e4a884e35a9e7fbf774865c27871be4a48
Harry Kraus <795900@seq.org>
2019-10-07 17:37:04 -0700

    Revert "Merge branch 'master' of https://github.com/795900/APCSPrinciples795900"
    This reverts commit 4a5a0958e80f9d3dcb8c47696d9d416240f1cc47, reversing
    changes made to 4987ccf55367f33a15ca1ce0ce67d40b9c9b2d85.

--- Lab 1004 Sorting/sketch.js
+++ Lab 1004 Sorting/sketch.js
@@ -6,11 +6,15 @@ function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(250, 250, 250);
-  var list = [3,7,2,5,8,9,1,4,6];
-  var count = list.length
-  console.log(list);
-  list = sort(list, count);
-  console.log(list);
+  var list = [3,7,2,5,8,9,1,4,6]
+  function swap(list,a,b){
+    var temp = list[a];
+    list[a] = list[b];
+    list[b] = temp;
+  }
 }
 
 //  The draw function is called @ 30 fps
+function draw() {
+
+}
