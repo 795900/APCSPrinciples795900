36ba7830dc98178b36d75691378042b4e7a3c26b
Harry Kraus <795900@seq.org>
2019-10-15 23:38:00 -0700

    Finish BubbleSort

--- Lab 1015 BubbleSort/sketch.js
+++ Lab 1015 BubbleSort/sketch.js
@@ -2,15 +2,15 @@
 // 	1011 BubbleSort
 //  This is a comment
 //  The setup function function is called once when your program begins
+var listLength = 100
+var list = []
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  var listLength = 10
-  var list = []
-  for (i = 0; i = 10; i++){
-    list.push(random(100))
+  for (n = 0; n < listLength; n++){
+    list.push(random(1000))
   }
   for (i=0; i<list.length; i++) {
     for (j=0; j<list.length; j++){
