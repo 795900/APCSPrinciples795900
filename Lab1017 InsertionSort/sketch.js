6de03ce558e0106dfa3874c2184685764e213ca4
795900 <795900@seq.org>
2019-10-18 08:50:27 -0700

    Finish InsertionSort

--- Lab1017 InsertionSort/sketch.js
+++ Lab1017 InsertionSort/sketch.js
@@ -1,5 +1,5 @@
 //  Harry Kraus
-// 	1011 BubbleSort
+// 	1011 InsertionSort
 //  This is a comment
 //  The setup function function is called once when your program begins
 var listLength = 10
@@ -10,23 +10,23 @@ function setup() {
   background(5, 5, 5);
   fill(200, 30, 150);
   for (n = 0; n < listLength; n++){
-    list.push(random(10))
+    list.push(random(100))
   }
-  for (i=1; i<list.lengt; i++) {
+  for (i=1; i<list.length; i++) {
     for (j=i; j>0; j--){
       if (list[j]<list[j-1]) {
-        swap(j);
+        swap(j)
+      }
+    }
   }
-}
-}
 console.log(list);
 }
-
 function swap(number){
-  temp = list[number];
+  var temp = list[number];
   list[number] = list[number-1];
   list[number-1] = temp;
 }
+
 //  The draw function is called @ 30 fps
 function draw() {
 
