05022b579190ecdc273d216220158a33c51a1e78
795900 <795900@seq.org>
2019-11-01 08:50:11 -0700

    Moving Head 1101

--- Project 1031 SnakeGame/head.js
+++ Project 1031 SnakeGame/head.js
@@ -1,23 +1,11 @@
 class Head{
-  constructor(x,y,vel){
+  constructor(x,y){
     this.x = x
     this.y = y
-    this.vel = vel
   }
 
-    update(){
-      if keyPressed()
-      this.x++
-    }
-
     render(){
-      fill(0,250,0)
+      fill(100,250,100)
       rect(this.x, this.y, 20, 20)
     }
-
-    run(){
-      this.update();
-      this.render();
-    }
-
 }
--- Project 1031 SnakeGame/sketch.js
+++ Project 1031 SnakeGame/sketch.js
@@ -2,26 +2,49 @@
 // 	Date or version number
 //  This is a comment
 //  The setup function function is called once when your program begins
+
 var head
+var xSquare
+var ySquare
 function setup() {
+  frameRate(15)
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadHead();
-
-
+  xSquare = width/40
+  ySquare = height/40
 }
 
+
 //  The draw function is called @ 30 fps
+
 function draw() {
-runHead();
+  background(5,5,5)
+  loadHead();
+  runHead();
+  moveHead();
 }
 
 function loadHead(){
-  head = new Head(width/2, height/2, 1);
+  head = new Head(xSquare*20, ySquare*20);
 }
 
 function runHead(){
-  head.run();
+  head.render();
+}
+
+function moveHead(){
+  if (keyCode === UP_ARROW){
+    ySquare--
+  }
+  if (keyCode === DOWN_ARROW){
+    ySquare++
+  }
+  if (keyCode === RIGHT_ARROW){
+    xSquare++
+  }
+  if(keyCode === LEFT_ARROW){
+    xSquare--
+  }
 }
