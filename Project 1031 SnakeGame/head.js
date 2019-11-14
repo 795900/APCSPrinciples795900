d1aa0e8b0297f54fb914176fe47823e932d0b669
795900 <795900@seq.org>
2019-11-14 09:50:55 -0800

    CheckEdges Working

--- Project 1031 SnakeGame/head.js
+++ Project 1031 SnakeGame/head.js
@@ -5,10 +5,17 @@ class Head{
   }
 
   checkEdges(){
-    for (i = body.length-1; i<=0; i--){
-      if (this.x === body[i].x && this.y === body[i].y){
-        console.log("working")
+    if (body.length>=1){
+      for (i = body.length-1; i>=0; i--){
+        if (this.x === body[i].x && this.y === body[i].y){
+          if (this.x != body[1].x || this.y != body[1].y){
+          console.log("working")
+          }
+        }
       }
+    }
+    if (this.x > width || this.x < 0 || this.y > height || this.y < 0){
+      console.log("working")
     }
   }
 
@@ -16,9 +23,9 @@ class Head{
       fill(100,250,100)
       rect(this.x, this.y, 20, 20)
     }
-}
 
   run(){
       this.checkEdges()
       this.render()
     }
+}
