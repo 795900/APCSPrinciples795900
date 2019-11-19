378808b2eae33f5569119bd231f4cc43134f17d0
795900 <795900@seq.org>
2019-11-19 09:52:17 -0800

    Server

---
+++ Project 1031 SnakeGameWithImages/food.js
@@ -0,0 +1,23 @@
+class Food{
+  constructor(x,y){
+    this.x = x
+    this.y = y
+  }
+
+
+  run(){
+    this.update()
+    this.render()
+  }
+
+  update(){
+    if (xSquare === this.x && this.y === ySquare){
+      this.x = round(random(width/20-1))
+      this.y = round(random(width/20-1))
+    }
+  }
+  render(){
+    fill(250,0,0)
+    image(img, this.x*20, this.y*20)
+  }
+}
